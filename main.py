98770dce36e28fa78f21ac48646b5ca84cbf962b
Hector Carvajal de los Reyes <hectordico@hotmail.com>
2026-02-27 07:18:24 -0500

    modificacion en el codigo

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 # CalculadorDeNotas
-nombre = input("Nombre del estudiante")
-nota1 = float(input("ingrese primnera nota"))
-nota2 = float(input("ingrese segunda nota"))
-nota3 = float(input("ingrese tercera nota"))
 
-# Promedio 
-promedio = ( nota1 + nota2 + nota3) / 3
-print(f"El promedio del estudiantes {nombre}, es: {promedio}" )
+nombre_del_estudiante = input("nombre del estudiante")
+grado_del_estudiante = input("grado del estudiante")
+notas = []
 
-# Aprobo o no 
-if promedio >= 70:
-    print("aprobado :)")
-else :
-    print("reprobo :()")
